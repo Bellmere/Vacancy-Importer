5bc4091eafb2cb832753f036c6230c908318f264
Bellmere <therise00@gmail.com>
2025-04-26 10:28:14 +0300

    Update VacancyModel.php

--- inc/API/Vacancy/model/VacancyModel.php
+++ inc/API/Vacancy/model/VacancyModel.php
@@ -2,7 +2,26 @@
 
 namespace VacanceImporter\API\Vacancy\model;
 
-class VacancyModel
-{
+class VacancyModel {
+    private $wpdb;
+
+    public function __construct($post_id) {
+        global $wpdb;
+        $this->wpdb = $wpdb;
+        $this->post_id = $post_id;
+
+        // Get the terms
+        $terms = wp_get_post_terms($this->post_id, 'featured');
+
+        // Ensure terms exist and assign the name of the first term
+        $this->term_name = (!empty($terms) && is_array($terms) && isset($terms[0]->name)) ? $terms[0]->name : null;
+    }
+
+    public function get_vacancies($city = '', $per_page = 10, $offset = 0) {
+        $query_args = [];
+        $city_filter = '';
+
+        
+    }
 
 }
